6c13a3376d73a12867bae59181ead1eb5bb173e6
Md. Shofiul Islam <ytshofiul@gmail.com>
2024-08-21 19:18:55 +0600

    issue #7: login implemented

---
+++ src/hooks/graphql/useLoginUser.ts
@@ -0,0 +1,37 @@
+"use client";
+
+import { gql, useMutation, useQuery } from "@apollo/client";
+import { useRouter } from "next/navigation";
+
+const LOGIN_USER = gql`
+    mutation Login($email: String!, $password: String!) {
+        loginUser(loginUser: { email: $email, password: $password }) {
+            jwtToken
+        }
+    }
+`;
+
+export const useLoginUser = () => {
+    const router = useRouter();
+    const [login, { error, data, loading }] = useMutation(LOGIN_USER);
+
+    const handleLogin = async (email: string, password: string) => {
+        try {
+            const { data } = await login({ variables: { email, password } });
+            if (data && data.loginUser && data.loginUser.jwtToken) {
+                //localStorage.setItem("token", data.loginUser.jwtToken);
+
+                router.push("/products");
+            }
+        } catch (err) {
+            console.error("Login failed:", err);
+        }
+    };
+
+    return {
+        handleLogin,
+        loading,
+        error,
+        data,
+    };
+};
